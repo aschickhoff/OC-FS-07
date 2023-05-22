3c811452a8d75a35cb51a1d9c8db2cce2245f5c2
aschickhoff <achaville@hotmail.de>
2023-05-22 09:02:24 +0200

    adding riders

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root to: "home#index"
   get "/photos", to: "photos#photos"
+  get "/riders", to: "riders#riders"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
