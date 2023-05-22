b00b7dcef3d474a8223e4f532d4c3a60c4f4ece4
aschickhoff <achaville@hotmail.de>
2023-05-22 09:35:35 +0200

    changing amount of displayed photos and made them clickable

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def photos
     flickr_client = FlickrClient.new(api_key: ENV['FLICKR_API_KEY'])
-    photos = flickr_client.fetch_photos(tag: 'BoulderBikeTour, bikerace', limit: 40, page: params[:page])
-    @photos = Kaminari.paginate_array(photos).page(params[:page]).per(10)
+    photos = flickr_client.fetch_photos(tag: 'BoulderBikeTour, bikerace', page: params[:page])
+    @photos = Kaminari.paginate_array(photos).page(params[:page]).per(40)
   end
 end
--- app/services/flickr_client.rb
+++ app/services/flickr_client.rb
@@ -5,11 +5,10 @@ class FlickrClient
     FlickRaw.api_key = api_key
   end
 
-  def fetch_photos(tag:, limit:, page:)
-    per_page = limit # Number of photos per page
+  def fetch_photos(tag:, page:)
     page_number = page || 1 # Current page number (default: 1)
 
-    flickr.photos.search(tags: tag, per_page: per_page, page: page_number).map do |photo|
+    flickr.photos.search(tags: tag, page: page_number).map do |photo|
       {
         url: FlickRaw.url(photo),
         title: photo.title,
