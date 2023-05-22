ea74ea0aeab2e072922ff042f466fa4787f2192e
aschickhoff <achaville@hotmail.de>
2023-05-22 08:47:41 +0200

    adding pagination

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   def photos
     flickr_client = FlickrClient.new(api_key: ENV['FLICKR_API_KEY'])
-    @photos = flickr_client.fetch_photos(tag: 'BoulderBikeTour, bikerace', limit: 40)
+    photos = flickr_client.fetch_photos(tag: 'BoulderBikeTour, bikerace', limit: 40, page: params[:page])
+    @photos = Kaminari.paginate_array(photos).page(params[:page]).per(10)
   end
 end
--- app/services/flickr_client.rb
+++ app/services/flickr_client.rb
@@ -5,8 +5,11 @@ class FlickrClient
     FlickRaw.api_key = api_key
   end
 
-  def fetch_photos(tag:, limit:)
-    flickr.photos.search(tags: tag, per_page: limit).map do |photo|
+  def fetch_photos(tag:, limit:, page:)
+    per_page = limit # Number of photos per page
+    page_number = page || 1 # Current page number (default: 1)
+
+    flickr.photos.search(tags: tag, per_page: per_page, page: page_number).map do |photo|
       {
         url: FlickRaw.url(photo),
         title: photo.title,
